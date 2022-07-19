83c2d6c4a23e4f9f8dcb13438d98159e9a1def5d
zhou-jinhong <2919918154@qq.com>
2022-07-19 10:03:26 +0800

    堆和堆的应用

--- Heap/test.c
+++ Heap/test.c
@@ -57,10 +57,77 @@ void HeapSortTest()//堆排序
 
 }
 
+void PrintTopK(HPDataType* a,int size,int k)
+{
+	//方法一：整体建堆
+	//这种方法，大数建大堆，小数建小堆
+	for (int i = (size - 1 - 1) / 2; i > 0; i--)
+	{
+		AdjustDwon(a, size, i);
+	}
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ",a[i]);
+		HeapPop(a);
+	}
+
+	//方法二：对前k个建堆
+	HPDataType* KMinHeap = (HPDataType*)malloc(sizeof(HPDataType) * k);
+	assert(KMinHeap);
+	//取出前k个数
+	for (int i = 0; i < k; i++)
+	{
+		KMinHeap[i] = a[i];
+	}
+	//建堆
+	for (int i = (k - 1 - 1) / 2; i >= 0; --i)
+	{
+		AdjustDwon(KMinHeap, k, i);
+	}
+	//用剩下的size-k个数比较，大于就入堆
+	for (int i = k; i < size; i++)
+	{
+		if (a[i] > KMinHeap[0])
+		{
+			KMinHeap[0] = a[i];
+			AdjustDwon(KMinHeap, k, 0);
+		}
+	}
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", KMinHeap[i]);
+	}
+}
+
+//topk问题
+void TestTopk()
+{
+	int n = 10000;
+	int* a = (int*)malloc(sizeof(int) * n);
+	assert(a);
+	srand(time(0));
+	for (int i = 0; i < n; ++i)
+	{
+		a[i] = rand() % 1000000;
+	}
+	a[5] = 1000000 + 1;
+	a[1231] = 1000000 + 2;
+	a[531] = 1000000 + 3;
+	a[5121] = 1000000 + 4;
+	a[120] = 1000000 + 5;
+	a[99] = 1000000 + 6;
+	a[0] = 1000000 + 7;
+	a[76] = 1000000 + 8;
+	a[423] = 1000000 + 9;
+	a[3144] = 1000000 + 10;
+	PrintTopK(a, n, 10);
+}
+
 int main(void)
 {
 	//Test();
-	HeapSortTest();
+	/*HeapSortTest();*/
+	TestTopk();
 
 	return 0;
 }
